7fbeeca9b974a13be31b2745984c08e20d2ec567
Carlos Sosa <isek7or@gmail.com>
2023-11-21 20:05:06 -0800

    create and new routes complete

---
+++ views/404.jsx
@@ -0,0 +1,18 @@
+const React = require('react')
+const Default = require('./layouts/Default')
+
+function error404() {
+    return (
+        <Default>
+            <main>
+                <h1>404: PAGE NOT FOUND</h1>
+                <p>Oops, sorry, we can't find this page!</p>
+            </main>
+            <div className="backButton">
+                <a href="/breads"><button>Go back to the index</button></a>
+            </div>
+        </Default>
+    )
+}
+
+module.exports = error404
