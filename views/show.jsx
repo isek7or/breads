7598a0548aaa33c12387542264cb12ee1d477906
Carlos Sosa <isek7or@gmail.com>
2023-12-05 19:20:10 -0800

    added bakers

--- views/show.jsx
+++ views/show.jsx
@@ -15,11 +15,12 @@ function Show({ bread, index }) {
                 have gluten.
             </p>
             <img src={bread.image} alt={bread.name} />
-            <li><a href="/breads">Go home</a></li>
+            <p>Baked by {bread.baker}</p>
+            <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
             <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
                 <input type='submit' value="DELETE" />
             </form>
-            <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
+            <li><a href="/breads">Go home</a></li>
         </Default>
     )
 }
