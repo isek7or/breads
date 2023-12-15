81fd2b585586e814fc3e81622c7030b09796a8d1
Carlos Sosa <isek7or@gmail.com>
2023-12-14 19:00:24 -0800

    Added BreadCRUD Hooks

--- models/baker.js
+++ models/baker.js
@@ -24,6 +24,14 @@ bakerSchema.virtual('breads', {
     foreignField: 'baker'
 })
 
+// hooks 
+bakerSchema.post('findOneAndDelete', function () {
+    Bread.deleteMany({ baker: this._conditions._id })
+        .then(deleteStatus => {
+            console.log(deleteStatus)
+        })
+})
+
 // Model and Export
 const Baker = mongoose.model('Baker', bakerSchema)
 
